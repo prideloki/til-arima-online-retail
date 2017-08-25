40701f29929992964f51bfe07544b55d067378ba
Thanakom Sangnetra <thanakom.sangnetra@hotelquickly.com>
2017-08-25 09:57:03 +0700

    adjust seasonal parameter

--- online-retail-ARIMA.py
+++ online-retail-ARIMA.py
@@ -24,7 +24,7 @@
 # 
 # 
 
-# In[4]:
+# In[174]:
 
 import pandas as pd
 import numpy as np
@@ -36,104 +36,104 @@ import operator
 import statsmodels.api as sm
 
 
-# In[5]:
+# In[175]:
 
 online_retail = pd.read_excel('data/Online Retail.xlsx')
 
 
-# In[6]:
+# In[176]:
 
 online_retail.describe()
 
 
-# In[7]:
+# In[177]:
 
 online_retail.head()
 
 
-# In[8]:
+# In[178]:
 
 online_retail['InvoiceDate'] = online_retail['InvoiceDate'].astype('datetime64[ns]')
 online_retail['TotalPrice'] = online_retail['Quantity'] * online_retail['UnitPrice']
 
 
-# In[9]:
+# In[179]:
 
 online_retail.head()
 
 
-# In[10]:
+# In[180]:
 
 online_retail.info()
 
 
-# In[11]:
+# In[181]:
 
 (online_retail['CustomerID'].isnull()).any()
 
 
-# In[12]:
+# In[182]:
 
 online_retail[online_retail['CustomerID'].isnull()]
 
 
-# In[13]:
+# In[183]:
 
 #calculate revenue? total sum of the price
 online_retail.set_index('InvoiceDate', inplace=True)
 
 
-# In[14]:
+# In[184]:
 
 # http://pandas.pydata.org/pandas-docs/stable/timeseries.html#offset-aliases
-y = online_retail['TotalPrice'].resample('D').sum()
+y = online_retail['TotalPrice'].resample('W').sum()
 
 
-# In[15]:
+# In[185]:
 
 y.head()
 
 
-# In[16]:
+# In[186]:
 
 y = y.fillna(y.bfill())
 
 
-# In[17]:
+# In[187]:
 
 y.head()
 
 
-# In[18]:
+# In[188]:
 
 y.isnull().any()
 
 
-# In[19]:
+# In[189]:
 
 y.plot(figsize=(15,6))
 plt.show()
 
 
-# In[20]:
+# In[217]:
 
 p = d = q = range(0, 2)
 
 pdq = list(itertools.product(p, d, q))
 
 # try adjust the `s` parameter
-s = 1
+s = 4
 seasonal_pdq = [(x[0], x[1], x[2], s) for x in list(itertools.product(p, d, q))]
 
 
-# In[21]:
+# In[218]:
 
 print('Example of parameter conbination for Seasonal ARIMA')
 print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
 print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[2]))
 
 
-# In[22]:
+# In[219]:
 
 warnings.filterwarnings('ignore')
 history = {}
@@ -155,23 +155,23 @@ for param in pdq:
 
 # Get the combination that results the minimum AIC
 
-# In[23]:
+# In[220]:
 
 sorted_x = sorted(history.items(), key=operator.itemgetter(1))
 
 
-# In[24]:
+# In[221]:
 
 param, param_seasonal =  sorted_x[0][0][0], sorted_x[0][0][1]
 
 
-# In[25]:
+# In[222]:
 
 print(param)
 print(param_seasonal)
 
 
-# In[26]:
+# In[223]:
 
 model = sm.tsa.statespace.SARIMAX(y,
                          order = param,
@@ -182,12 +182,12 @@ model = sm.tsa.statespace.SARIMAX(y,
 results = model.fit()
 
 
-# In[27]:
+# In[224]:
 
 print(results.summary())
 
 
-# In[28]:
+# In[225]:
 
 # http://www.statsmodels.org/dev/generated/statsmodels.tsa.statespace.sarimax.SARIMAXResults.plot_diagnostics.html
 results.plot_diagnostics(lags=1, figsize=(15,6))
@@ -201,18 +201,18 @@ plt.show()
 
 # ### One-step ahead forecast
 
-# In[29]:
+# In[226]:
 
-start_date = '2011-10-01'
+start_date = '2011-01-02'
 pred = results.get_prediction(start=pd.to_datetime(start_date), dynamic=False)
 
 
-# In[30]:
+# In[227]:
 
 pred_ci = pred.conf_int()
 
 
-# In[31]:
+# In[228]:
 
 ax = y['2011':].plot(label='observed')
 
@@ -231,7 +231,7 @@ plt.legend()
 plt.show()
 
 
-# In[32]:
+# In[229]:
 
 y_forecasted = pred.predicted_mean
 y_truth = y[start_date:]
@@ -243,14 +243,14 @@ print('The Mean Squared Error of our forecasts is {}'.format(round(mse,2)))
 
 # ### Dynamic forecast
 
-# In[33]:
+# In[230]:
 
 pred_dynamic = results.get_prediction(start=pd.to_datetime(start_date), dynamic=True)
 
 pred_dynamic_ci = pred_dynamic.conf_int()
 
 
-# In[34]:
+# In[231]:
 
 ax = y['2011':].plot(label='observed')
 
@@ -271,7 +271,7 @@ plt.legend()
 plt.show()
 
 
-# In[35]:
+# In[232]:
 
 y_forecasted = pred_dynamic.predicted_mean
 y_truth = y[start_date:]
@@ -284,16 +284,16 @@ print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
 
 # ## Visualizing Forecasts
 
-# In[36]:
+# In[235]:
 
-pred_uc = results.get_forecast(steps=7)
+pred_uc = results.get_forecast(steps=10)
 
 pred_ci = pred_uc.conf_int()
 
 
-# In[37]:
+# In[236]:
 
-ax = y.plot(label='observed', figsize=(20,15))
+ax = y.plot(label='observed', figsize=(15,10))
 
 pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
 ax.fill_between(pred_ci.index,
@@ -307,6 +307,16 @@ ax.set_ylabel('Sum of Total Price')
 
 plt.legend()
 plt.show()
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
